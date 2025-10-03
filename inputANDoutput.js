4d8a9196319e5363019501a93d39dea8194bd738
SanaOsman21 <sanaosman8922@gmail.com>
2025-10-03 20:44:48 +0530

    i/p and o/p methods explore

--- inputANDoutput.js
+++ inputANDoutput.js
@@ -1,8 +1,7 @@
-const prompt = require('prompt-sync')();
-// let name = prompt("Enter your name: ");
-// console.log("Hello, " + name);
+const prompt = require('prompt-sync')(); 
+let name = prompt("Enter your name: ");
+console.log("Hello, " + name);
+//altenative to alert in node js 
 console.log("This is a message");
 console.warn("This is a warning");
-console.error("This is an error");
-
-
+console.error("This is an error"); 
