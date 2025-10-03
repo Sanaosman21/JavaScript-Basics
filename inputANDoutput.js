fdd348b9ba67e37a61519f0cbcbb91643e886e61
SanaOsman21 <sanaosman8922@gmail.com>
2025-10-03 20:38:04 +0530

    input and output function explore

---
+++ inputANDoutput.js
@@ -0,0 +1,8 @@
+const prompt = require('prompt-sync')();
+// let name = prompt("Enter your name: ");
+// console.log("Hello, " + name);
+console.log("This is a message");
+console.warn("This is a warning");
+console.error("This is an error");
+
+
